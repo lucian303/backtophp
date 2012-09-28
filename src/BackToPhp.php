ef7f368897ac9420c581656f179a3b7c1581845b
Lucian Hontau <lucian303@gmail.com>
2012-09-28 00:31:28 -0700

    Starting to explore Smarty2 ...

--- src/BackToPhp.php
+++ src/BackToPhp.php
@@ -3,4 +3,26 @@
 class BackToPhp
 {
 
+	public $templateEngine;
+
+	protected $templateEngineClassFile;
+	protected $templateEngineClass;
+
+	public function __construct($templateEngineClass, $templateEngineClassFile)
+	{
+		$this->setTemplateEngine($templateEngineClass, $templateEngineClassFile);
+	}
+
+	public function setTemplateEngine($templateEngineClass, $templateEngineClassFile)
+	{
+		require_once "$templateEngineClassFile";
+
+		$this->templateEngineClass = $templateEngineClass;
+		$this->templateEngine = new $templateEngineClass;
+		return $this;
+	}
+
 }
+
+$b2p = new BackToPhp("Smarty", '../../Smarty-2.6.27/libs/Smarty.class.php');
+var_dump($b2p->templateEngine);
