4868c5b457f0877a8d68b032260c4acec2a8ef94
Lucian Hontau <lucian303@gmail.com>
2012-09-28 01:52:04 -0700

    refactor tests

--- tests/BackToPhpTest.php
+++ tests/BackToPhpTest.php
@@ -5,15 +5,19 @@ require_once "../src/BackToPhp.php";
 class BackToPhpTest extends PHPUnit_Framework_TestCase
 {
 
-	protected function setUp() {}
+	protected $b2p;
+
+	protected function setUp()
+	{
+		$this->b2p = new BackToPhp();
+		$this->b2p->setTemplateEngine('Smarty', '../../Smarty-2.6.27/libs/Smarty.class.php');
+	}
 
 	protected function tearDown() {}
 
 	public function testSetSmartyAsEngine()
 	{
-		$b2p = new BackToPhp();
-		$b2p->setTemplateEngine('Smarty', '../../Smarty-2.6.27/libs/Smarty.class.php');
-		$this->assertTrue($b2p->templateEngine instanceof Smarty, "Smarty Templating Engine Set");
+		$this->assertTrue($this->b2p->templateEngine instanceof Smarty, "Smarty Templating Engine Set");
 	}
 
 }
