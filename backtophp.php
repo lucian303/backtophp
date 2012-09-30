fbfa5c30a3c06e99a322ecb4a8d98e46b92e70ac
Lucian Hontau <lucian303@gmail.com>
2012-09-30 12:06:16 -0700

    Some working but still pseudocode for the the php bootstrap / executable

--- backtophp.php
+++ backtophp.php
@@ -1,6 +1,13 @@
+#!/usr/bin/php
 <?php
 
 /**
  * Bootstrap startup file
  * This is going to be the program executable. Shebang should be set upon install
  */
+
+require_once 'src/BackToPhp.php';
+
+$b2p = new BackToPhp();
+// TODO: Initialize back to php from cli params or ini file, then run
+$b2p->run();
